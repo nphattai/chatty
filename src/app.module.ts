1d1585f06e3cb56c11f608685ead1b036813c5da
Tai Nguyen <nphattai98@gmail.com>
2023-03-29 18:33:35 +0700

    feat: get me

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -9,7 +10,8 @@ import { PrismaModule } from './prisma/prisma.module';
       isGlobal: true
     }),
     PrismaModule,
-    AuthModule
+    AuthModule,
+    UserModule
   ]
 })
 export class AppModule {}
