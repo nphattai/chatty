6739d2e7e7e26705d567099bc1c6e68ad402c8b0
Tai Nguyen <nphattai98@gmail.com>
2023-04-03 15:11:43 +0700

    feat: add prisma, connect with postgres sql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
 import { PostModule } from './post/post.module';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [PostModule]
+  imports: [
+    PostModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DATABASE_URL: Joi.string().required()
+      })
+    }),
+    PrismaModule
+  ]
 })
 export class AppModule {}
--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { PostService } from './post.service';
+import { CreatePostDto } from './dto/create-post.dto';
 
 @Controller('posts')
 export class PostController {
   constructor(private postService: PostService) {}
 
+  @Post()
+  async createPost(@Body() dto: CreatePostDto) {
+    return this.postService.createPost(dto);
+  }
+
   @Get()
   async getPosts() {
     return this.postService.getPosts();
+  }
+
+  @Get('/:id')
+  async getPost(@Param('id') id: string) {
+    return this.postService.getPostById(Number(id));
   }
 }
