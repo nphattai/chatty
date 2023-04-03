2506033e5c993679c1417945c522476735f5139c
Tai Nguyen <nphattai98@gmail.com>
2023-04-03 16:43:03 +0700

    feat: add global prisma client exception

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,7 +2,6 @@ import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import * as bcrypt from 'bcrypt';
-import { Prisma } from '@prisma/client';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 import { LoginUserDto } from './dto';
@@ -12,59 +11,43 @@ export class AuthService {
   constructor(private prisma: PrismaService, private jwt: JwtService, private config: ConfigService) {}
 
   async create(dto: CreateUserDto) {
-    try {
-      const hashedPassword = await bcrypt.hash(dto.password, 10);
+    const hashedPassword = await bcrypt.hash(dto.password, 10);
 
-      const user = await this.prisma.user.create({
-        data: {
-          ...dto,
-          password: hashedPassword
-        }
-      });
-
-      delete user.password;
-
-      const token = await this.signToken(user.id, user.email);
-
-      return { user, token };
-    } catch (error) {
-      if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === 'P2002') {
-        throw new ForbiddenException('Invalid credentials');
-      } else {
-        throw error;
+    const user = await this.prisma.user.create({
+      data: {
+        ...dto,
+        password: hashedPassword
       }
-    }
+    });
+
+    delete user.password;
+
+    const token = await this.signToken(user.id, user.email);
+
+    return { user, token };
   }
 
   async login(dto: LoginUserDto) {
-    try {
-      const user = await this.prisma.user.findUnique({
-        where: {
-          email: dto.email
-        }
-      });
-
-      if (!user?.id) {
-        throw new ForbiddenException('Invalid credentials');
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email
       }
+    });
 
-      const isMatch = await bcrypt.compare(dto.password, user.password);
-      if (!isMatch) {
-        throw new ForbiddenException('Invalid credentials');
-      }
-
-      delete user.password;
-
-      const token = await this.signToken(user.id, user.email);
-
-      return { user, token };
-    } catch (error) {
-      if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === 'P2002') {
-        throw new ForbiddenException('Invalid credentials');
-      } else {
-        throw error;
-      }
+    if (!user?.id) {
+      throw new ForbiddenException('Invalid credentials');
     }
+
+    const isMatch = await bcrypt.compare(dto.password, user.password);
+    if (!isMatch) {
+      throw new ForbiddenException('Invalid credentials');
+    }
+
+    delete user.password;
+
+    const token = await this.signToken(user.id, user.email);
+
+    return { user, token };
   }
 
   async signToken(userId: number, email: string) {
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma/prisma.service';
 import { ValidationPipe } from '@nestjs/common';
+import { PrismaClientExceptionFilter } from './prisma-client-exception/prisma-client-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,9 @@ async function bootstrap() {
   await prismaService.enableShutdownHooks(app);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
 
   await app.listen(3000);
 }
