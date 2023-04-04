dfef74083234e035103972ca58b19ba474b95611
Tai Nguyen <nphattai98@gmail.com>
2023-04-04 12:20:55 +0700

    feat: use prisma as global module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { JwtStrategy } from 'src/auth/strategy/jwt.strategy';
-import { PrismaModule } from 'src/prisma/prisma.module';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
-  imports: [PrismaModule],
   controllers: [UserController],
   providers: [UserService, JwtStrategy]
 })
